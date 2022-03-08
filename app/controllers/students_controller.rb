d0a6e5d1debc134e1f616f072c6bca2e3484c708
Leighton <leighton.l14@gmail.com>
2022-03-09 02:11:15 +1100

    Finished!

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,16 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    #redirect to show page
+    redirect_to student_path(params[:id])
+    # redirect_to student_path(@students)
   end
 
   private
